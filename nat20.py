c2cb4b2665078dd7367c0b572c77a2e8a149b362
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-02 08:35:19 -0400

    Read from character sheet in order and load in order

--- nat20.py
+++ nat20.py
@@ -3,6 +3,19 @@ import random
 import yaml
 from functools import partial
 import random,re
+from collections import OrderedDict
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
 
 class nat20_tk(Tkinter.Tk):
   def __init__(self,parent):
@@ -12,15 +25,16 @@ class nat20_tk(Tkinter.Tk):
 
   def initialize(self):
     sheet_stream = open("sheet.yaml", 'r')
-    sheet = yaml.load(sheet_stream)
+    sheet = ordered_load(sheet_stream)
 
     for character, rolls in sheet.iteritems():
       labelframe = Tkinter.LabelFrame(self, text=character)
       labelframe.pack(fill="both", expand="yes")
+      column_height = len(rolls)/3 + 1
       for index, (roll_text, roll) in enumerate(rolls.iteritems()):
         roll_command = partial(self.do_roll, roll)
         button = Tkinter.Button(labelframe, text = roll_text + " Roll", command = roll_command)
-        button.grid(row=index/3, column=index%3)
+        button.grid(row=index%column_height, column=index/column_height, sticky=Tkinter.W)
 
     self.text = Tkinter.Text(self)
     self.text.insert(Tkinter.INSERT, "Results:")
